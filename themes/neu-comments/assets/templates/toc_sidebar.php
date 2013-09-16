3266e91022be834b3a0f1b71629ac2381de6173d
Matthew Willse <mwillse@gmail.com>
2013-09-16 14:08:30 -0400

    add draft "Neu" theme. intended for collection of independent documents rather than chapters of a single document. cleaner comment nav & aesthetics. prototyping page meta.

---
+++ themes/neu-comments/assets/templates/toc_sidebar.php
@@ -0,0 +1,45 @@
+<!-- toc_sidebar.php -->
+
+<div id="navigation">
+
+<div id="toc_sidebar" class="sidebar_container">
+
+
+<?php
+    // @neu
+    // toc_sidebar.php rewritten to serve as doc meta data rather than navigation across site
+?>
+
+
+<div class="sidebar_minimiser">
+
+    <div class="sidebar_contents_wrapper">
+
+        <div class="neu-meta">
+            <h2>About this Document</h2>
+
+            <p><strong>Created: </strong>9/2/2013</p>
+            <p><strong>Comments Close: </strong>12/17/2013</p>
+            <p><strong>Author: </strong>XYZ Committee</p>
+            <p><strong>Description: </strong>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+
+            <h2>To Participate</h2>
+            <ul>
+                <li>Review the entire document.</li>
+                <li>Browse comments on paragraphs of interest.</li>
+                <li>Reply to whole document, individuals paragraphs, or comments by your peers.</li>
+            </ul>
+                
+        </div>
+
+    </div><!-- /sidebar_contents_wrapper -->
+
+</div><!-- /sidebar_minimiser -->
+
+
+
+</div><!-- /toc_sidebar -->
+
+</div><!-- /navigation -->
+
+
